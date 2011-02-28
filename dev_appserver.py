e0662833c5370cbfda5c333689db3d568bd0cafb
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2011-02-28 13:41:13 +0100

    removed dev_appserver, functionality is already present in manage.py

--- dev_appserver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-os.environ['SERVER_SOFTWARE'] = 'development'
-
-from werkzeug import run_simple
-from application import application
-
-run_simple('localhost', 5000, application, use_debugger=False, 
-           use_reloader=True, threaded=False, processes=1)
