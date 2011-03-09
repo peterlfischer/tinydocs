f3f678c1239b6b3dc2a60502f4c2186f8df11519
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2011-03-09 09:00:30 +0100

    path to indexes changed

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 from flask import Flask
 import os
-import sys
-
-# To enable production state do this:
-# $ export TINYDOCS_SETTINGS=./settings.prod.cfg
 
 app = Flask('tiny')
 if os.environ.get('TINYDOCS_SETTINGS'):
+    # We're setting that variable in the tiny.wsgi used by apache
+    # Another way to enable production state do this:
+    # $ export TINYDOCS_SETTINGS=./settings.prod.cfg
     app.config.from_envvar('TINYDOCS_SETTINGS')    
 else:
     app.config.from_pyfile('settings.dev.cfg')
+app.config['INDEX_PATH'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'indexes', app.config['MODE'])
 
-    
