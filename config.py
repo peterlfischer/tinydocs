bc55fe6d99888a0078f6ed388c9aaceae6735558
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2011-03-14 11:16:44 +0100

    abs. path to index path.

--- config.py
+++ config.py
@@ -12,3 +12,6 @@ if os.environ.get('TINYDOCS_SETTINGS'):
 else:
     app.config.from_pyfile('settings.dev.cfg')
     # os.environ['REMOTE_USER'] = 'foo'
+SITE_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['INDEX_PATH'] = os.path.join(SITE_ROOT, app.config['INDEX_PATH'])
+
